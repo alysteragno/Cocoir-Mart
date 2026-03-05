7d85a7b936b78d28da5321e178ae8157001281c5
Alyster <alysteragno@gmail.com>
2026-03-05 22:51:40 +0800

    fix(route): admin access to order using path route

--- app/orders/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function OrdersPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Transaction History</h1>
-      <p>List of past orders will appear here.</p>
-    </div>
-  )
-}
